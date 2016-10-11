f3e4038cd38cbc2d814b060f1dc6a1981ee1fbd2
Thomas Harlan <thomasrharlan@gmail.com>
2016-10-10 23:23:36 -0400

    Added palette framework

--- scripts/controllers/palette.js
+++ scripts/controllers/palette.js
@@ -2,5 +2,15 @@
 
 angular.module('swatcher')
     .controller('paletteCtrl', function ($scope, $routeParams, $location) {
+    	$scope.paletteBackgroundColor1 = {"background-color": "#eeeeee"}
+    	$scope.paletteBackgroundColor2 = {"background-color": "#dddddd"}
+    	$scope.paletteBackgroundColor3 = {"background-color": "#cccccc"}
+    	$scope.paletteBackgroundColor4 = {"background-color": "#bbbbbb"}
+    	$scope.paletteBackgroundColor5 = {"background-color": "#aaaaaa"}
 
+    	$scope.paletteColor1 = "#eeeeee"
+    	$scope.paletteColor2 = "#dddddd"
+    	$scope.paletteColor3 = "#cccccc"
+    	$scope.paletteColor4 = "#bbbbbb"
+    	$scope.paletteColor5 = "#aaaaaa"
 });
